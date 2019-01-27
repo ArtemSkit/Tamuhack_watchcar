ff39b8b452d5aa600ba6c7c6dc98cd2fd19e729d
Artem <artemskitenko93@gmail.com>
2019-01-27 03:05:49 -0600

    ui test 40

--- watchcar.py
+++ watchcar.py
@@ -52,20 +52,20 @@ def exchange():
 @app.route('/vehicle', methods=['GET'])
 def vehicle():
     # access our global variable to retrieve our access tokens
-    global access
     try:
         access = session.get('access', None)
         # the list of vehicle ids
         vehicle_ids = smartcar.get_vehicle_ids(
             access['access_token'])['vehicles']
-
-        # instantiate the first vehicle in the vehicle id list
-        vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
-
-        resp = vehicle.info()
-        session['vehicle'] = vehicle
     except Exception as e:
         return str(e)
+        # instantiate the first vehicle in the vehicle id list
+    vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
+
+    resp = vehicle.info()
+    session['vehicle'] = vehicle
+    # except Exception as e:
+    #     return str(e)
     # resp.update(vehicle.odometer())
     # resp['data']['location'] = (vehicle.location())
     return render_template('info.html', data=str(resp))
